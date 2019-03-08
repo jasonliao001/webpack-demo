ad20734f000b65e9d3c6716896e2fe788c8f4374
jasonliao1992@163.com <407077499@qq.com>
2019-03-08 14:12:38 +0800

    change

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -1,8 +1,27 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
-  }
+    path: path.resolve(__dirname, './dist'),
+    chunkFilename: '[id].chunk.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: ['css-loader']
+        })
+        // use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name]_[contenthash:8].css'
+    })
+  ]
 };
